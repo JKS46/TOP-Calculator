d07b14d44d6d88632fe0c56e781ad9f62d0e579e
Jaswanth Kishore S <93701274+JKS46@users.noreply.github.com>
2022-12-07 23:57:52 +0530

    starting script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-/*  */
+const container = document.querySelector('#container');
+
+container.addEventListener("click",calculator);
+
+function calculator(e){
+    const target = e.target;
+    console.log(target);
+   
+}
+
+function reset(){
+    console.log("clear");
+}
+
+function backspace(){
+    console.log("backspace");
+}
