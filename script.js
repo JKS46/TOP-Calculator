5f1b5c31c831559b5068176b160f4134bb94836b
Jaswanth Kishore S <93701274+JKS46@users.noreply.github.com>
2022-12-10 22:58:41 +0530

    operands don't go back to back now,changed x to *

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const container = document.querySelector('#container');
 const display = document.querySelector('#display');
+let doubleOperator = 0;
 
 container.addEventListener("click",calculator);
 
 function calculator(e){
     const target = e.target;
 
+    /* key press effects */
     target.classList.add("pressed");
-
     setTimeout(function(){
         target.classList.remove("pressed");
     }
     ,250);
+
+    if(display.textContent.length >27){
+        alert("This is too long");
+        return
+    }
 
     if(target.textContent == "Clear"){
         reset();
@@ -23,6 +29,17 @@ function calculator(e){
         equal();
     }
     else{
+        if(target.classList.contains("op")){
+            doubleOperator++;
+        }
+        else{
+            doubleOperator=0;
+        }
+
+        if(doubleOperator>1){
+            backspace();
+        }
+
         if(display.textContent == "0"){
             display.textContent = "";
         }
@@ -32,12 +49,10 @@ function calculator(e){
 /* touch and key both? */
 
 function reset(){
-    console.log("clear");
     display.textContent = "0";
 }
 
 function backspace(){
-    console.log("backspace");
     display.textContent = display.textContent.slice(0,-1);
     if(display.textContent == ""){
         display.textContent = "0";
@@ -45,5 +60,6 @@ function backspace(){
 }
 
 function equal(){
+    console.log("equals");
 
 }
