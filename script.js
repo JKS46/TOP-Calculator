9cdb0fade7f45599a5de8c7b425f0b6600377b68
Jaswanth Kishore S <93701274+JKS46@users.noreply.github.com>
2022-12-10 10:36:30 +0530

    added display functionality and button animation

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 const container = document.querySelector('#container');
+const display = document.querySelector('#display');
 
 container.addEventListener("click",calculator);
 
 function calculator(e){
     const target = e.target;
-    console.log(target);
-   
+
+    target.classList.add("pressed");
+
+    setTimeout(function(){
+        target.classList.remove("pressed");
+    }
+    ,250);
+
+    if(target.textContent == "Clear"){
+        reset();
+    }
+    else if(target.textContent == "⌫"){
+        backspace();
+    }
+    else if(target.textContent == "="){
+        equal();
+    }
+    else{
+        if(display.textContent == "0"){
+            display.textContent = "";
+        }
+        display.textContent += target.textContent;
+    }
 }
-/* touch and key function included */
+/* touch and key both? */
 
 function reset(){
     console.log("clear");
+    display.textContent = "0";
 }
 
 function backspace(){
     console.log("backspace");
+    display.textContent = display.textContent.slice(0,-1);
+    if(display.textContent == ""){
+        display.textContent = "0";
+    }
+}
+
+function equal(){
+
 }
